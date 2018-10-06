5b9c47c2c386824cad41db97fea0b02fa64687e6
Benjamin Soyka <bensoyka@icloud.com>
2018-10-06 14:57:47 -0700

    Add pyPI metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup
+import setuptools
 
-setup(name='averager',
-      version='1.0',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='averager',
+      version='1.0.1',
       description='A simple averager',
       url='http://github.com/bsoyka/averager',
       author='Benjamin Soyka',
       author_email='bensoyka@icloud.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=setuptools.find_packages(),
       license='GPL-3.0',
-      packages=['averager'],
       zip_safe=False)
