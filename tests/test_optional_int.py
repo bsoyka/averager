4be671b63cf9d1b2f00e7e482c092b5801517406
Ben Soyka <bensoyka@icloud.com>
2021-08-29 12:07:31 -0600

    :white_check_mark: Add tests for _optional_int

---
+++ tests/test_optional_int.py
@@ -0,0 +1,23 @@
+from py import test
+from pytest import mark
+from averager import _optional_int
+
+
+@mark.parametrize('test_input', [1, 2, 3, 4, 5])
+def test_int_to_int(test_input):
+    assert _optional_int(test_input) == test_input
+    assert type(_optional_int(test_input)) == int
+
+
+@mark.parametrize(
+    'test_input,expected', [(1.0, 1), (2.0, 2), (3.0, 3), (4.0, 4), (5.0, 5)]
+)
+def test_float_to_int(test_input, expected):
+    assert _optional_int(test_input) == expected
+    assert type(_optional_int(test_input)) == int
+
+
+@mark.parametrize('test_input', [1.2, 2.3, 3.4, 4.5])
+def test_float_to_float(test_input):
+    assert _optional_int(test_input) == test_input
+    assert type(_optional_int(test_input)) == float
