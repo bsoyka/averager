e39d6db6e4b82910bc6bfda62f4941301144e994
Ben Soyka <bensoyka@icloud.com>
2021-08-29 18:47:30 -0600

    :test_tube: Add tests for new median function

---
+++ tests/test_median.py
@@ -0,0 +1,35 @@
+from pytest import mark, raises
+
+from averager import median
+
+
+@mark.parametrize(
+    'values,expected',
+    [
+        ([1, 2, 3, 4, 5], 3),
+        ([1, 2, 3], 2),
+        ([1.5, 1.5, 1.5], 1.5),
+        ([3.21, 4.56, 12.34], 4.56),
+    ],
+)
+def test_median_odd_values_len(values, expected):
+    assert median(values) == expected
+    assert type(median(values)) == type(expected)
+
+
+@mark.parametrize(
+    'values,expected',
+    [([1, 2, 3, 4, 5, 6], 3.5), ([1, 2], 1.5), ([5, 1, 3], 3)],
+)
+def test_median_even_values_len(values, expected):
+    assert median(values) == expected
+    assert type(median(values)) == type(expected)
+
+
+def test_median_empty_list():
+    with raises(IndexError):
+        median([])
+
+
+def test_median_one_value():
+    assert median([1]) == 1
